9dbfa14732296e9f92db06b822f114d7b560a0ab
Kamil Pomykała <kamil@kamilpomykala.dev>
2026-06-14 21:41:44 +0200

    feat: Install Tailwind v4 and Fonts

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,14 @@ import { fileURLToPath, URL } from "node:url";
 
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import tailwindcss from "@tailwindcss/vite";
 
 // @ts-expect-error process is a nodejs global
 const host = process.env.TAURI_DEV_HOST;
 
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
-  plugins: [vue()],
+  plugins: [vue(), tailwindcss()],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
