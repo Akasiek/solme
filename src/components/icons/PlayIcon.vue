320c0cf9bfdc63e3d3fa7ae939870ac3c3f5a185
Kamil Pomykała <kamil@kamilpomykala.dev>
2026-06-18 18:59:07 +0200

    feat: Update Play and Pause icons to use currentColor for better styling

--- src/components/icons/PlayIcon.vue
+++ src/components/icons/PlayIcon.vue
@@ -1,5 +1,5 @@
 <template>
-  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" role="img">
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" role="img" fill="currentColor">
     <path d="M4 4l12 6-12 6z"/>
   </svg>
 </template>
