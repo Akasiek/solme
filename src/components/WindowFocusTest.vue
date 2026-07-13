f83ba6ad453b5eb0b20ad315106faeb84b5b488d
Kamil Pomykała <kamil@kamilpomykala.dev>
2026-07-13 13:26:03 +0200

    style: Improve formatting and readability in WindowFocusTest.vue
    - Adjust whitespace for consistency in imports and template structure.
    - Enhance readability of the template by formatting the div element.

--- src/components/WindowFocusTest.vue
+++ src/components/WindowFocusTest.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { usePageLeave, useIdle, useWindowFocus } from "@vueuse/core";
-import {computed} from "vue";
+import { computed } from "vue";
 
 const hasLeftThePage = usePageLeave();
 const isIdle = useIdle(5 * 1000);
@@ -13,9 +13,12 @@ const showPage = computed(() => {
 </script>
 
 <template>
-  <div v-if="showPage" class="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-80 text-white">
+  <div
+    v-if="showPage"
+    class="bg-opacity-80 fixed inset-x-0 top-0 z-50 flex h-12 items-center justify-center bg-black text-white"
+  >
     <div class="text-center">
-      <h1 class="text-2xl font-bold mb-4">You have left the page</h1>
+      <h1 class="mb-4 text-2xl font-bold">You have left the page</h1>
       <p class="mb-4">Please return to the page to continue using the application.</p>
     </div>
   </div>
