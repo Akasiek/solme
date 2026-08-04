f4b1e3931b56aa7a2ac6552467b63ec73417b293
Kamil Pomykała <kamil@kamilpomykala.dev>
2026-08-04 13:28:02 +0200

    refactor: Standardize typography styles in `AlbumCard` component
    - Updated font sizes and weights in `AlbumCard.vue` for consistent text styling.
    - Introduced new CSS variables (`--text-md`, `--text-md--line-height`) for reusable typography definitions.

--- src/components/Album/AlbumCard/AlbumCard.vue
+++ src/components/Album/AlbumCard/AlbumCard.vue
@@ -23,12 +23,12 @@ const releaseYear = computed(
     <div
       class="space-y-1 gap-x-2.5 rounded-md border border-zinc-800 px-2.5 py-3 transition-colors duration-300 ease-in-out group-hover:border-zinc-600"
     >
-      <h3 class="line-clamp-1 font-serif font-bold hover:underline" :title="album.name">{{ album.name }}</h3>
-      <p v-if="showReleaseYear" class="line-clamp-1 font-sans font-normal text-zinc-300">
+      <h3 class="line-clamp-1 font-serif text-md font-bold hover:underline" :title="album.name">{{ album.name }}</h3>
+      <p v-if="showReleaseYear" class="line-clamp-1 font-sans text-sm font-normal text-zinc-300">
         {{ releaseYear }}
       </p>
       <RouterLink v-else :to="{ name: 'artist', params: { artistId: album.artistId } }">
-        <h4 class="line-clamp-1 font-sans font-normal text-zinc-300 hover:underline" :title="album.artistName">
+        <h4 class="line-clamp-1 font-sans text-sm font-normal text-zinc-300 hover:underline" :title="album.artistName">
           {{ album.artistName }}
         </h4>
       </RouterLink>
